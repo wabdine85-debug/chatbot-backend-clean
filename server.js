ef969dc9d1e08f7e8dc0a2398d63204d5af162df
Wassim Abdine <nonowi@Wassims-iMac.fritz.box>
2025-09-26 11:26:20 +0200

    Fix: Punkt hinter URL entfernt

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.post("/chat", async (req, res) => {
           content: `Du bist Wisy, ein Beratungsassistent für PDB Aesthetic Room (kurz: PDB).
 Antworte stets freundlich, professionell und in maximal zwei Sätzen.
 Wenn der Nutzer nach E-Mail, Kontakt oder Termin fragt, gib IMMER nur diesen Link zum Kontaktformular an:
-https://palaisdebeaute.de/pages/contact .
+https://palaisdebeaute.de/pages/contact
 Erfinde niemals eine andere E-Mail-Adresse oder Telefonnummer.`,
         },
         { role: "user", content: userMessage },
